1ac1a9e8aae2ca53d48be0a502838d40474a5830
root <root@ip-172-31-89-163.ec2.internal>
2022-02-27 15:25:09 +0000

    worker 2 changes

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,13 @@ provider "aws" {
  region = "us-east-1"
 }
 
+<<<<<<< HEAD
 resource "aws_instance" "instance_example" {
  ami = "some_ami"
  instance_type = "t2.micro"
+=======
+provider "aws" {
+ profile = "default"
+ region = "us-east-1"
+>>>>>>> worker 2 changes
 }
